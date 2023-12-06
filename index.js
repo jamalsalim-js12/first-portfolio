3a0f0efb08b04184061b014f0b97bac4a8efa1e5
im-js12 <Jamalsalim452@gmail.com>
2023-12-06 19:15:38 +0000

    Contact Page Added

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ $(document).ready(() => {
     $("#navlist1").slideToggle();
   });
 });
+
+// const navlist = document.querySelector("#navlist1");
+// const navbar = document.querySelector(".fa-solid");
+
+// navbar.addEventListener("click", fadeToggle());
