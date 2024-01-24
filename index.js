4f181da2ea4092b72c905a14d16d27c290a5e831
Jamal Salim <Jamalsalim.js12@gmail.com>
2024-01-24 00:47:23 +0000

    90% Completed

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-$(document).ready(() => {
-  $(".fa-solid").click(() => {
-    $("#navlist1").slideToggle();
-  });
+const icon = document.querySelector(".fa-bars");
+
+const navlist = document.querySelector("#navlist");
+
+icon.addEventListener("click", () => {
+  if (navlist.style.display === "none" || navlist.style.display === "") {
+    navlist.style.display = "block";
+  } else {
+    navlist.style.display = "none";
+  }
 });
 
-// const navlist = document.querySelector("#navlist1");
-// const navbar = document.querySelector(".fa-solid");
+const typed = new Typed(".auto-type", {
+  strings: ["Web", "Full Stack Developer"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
 
-// navbar.addEventListener("click", fadeToggle());
+(function () {
+  emailjs.init("ssB6v88rswISwPAaT");
+})();
+
+document.getElementById("send").addEventListener("click", SendMail);
+
+function SendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs
+    .send("service_6t2tivr", "template_0bjrj9b", params)
+    .then(function (res) {
+      alert("Success! " + res.status);
+    });
+}
